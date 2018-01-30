28dce03a8d8eb5a080bb668f90bd5cc1a0e46804
rschreib <rschreib@mymail.mines.edu>
2018-01-29 17:06:35 -0700

    h

--- exercise2/exercise2.py
+++ exercise2/exercise2.py
@@ -10,17 +10,10 @@ import os
 import cv2
 import numpy as np
 
-import sys
-import subprocess
-def openImage(path):
-	imageViewer = {'linux':'eog','win32':'explorer','darwin':'open'}[sys.platform]
-	subprocess.run([imageViewer, path])
-
 camera = PiCamera()
 camera.rotation = 180
 
-
-# 1a
+# 1a  ##################################################################
 print("Look into Camera ...")
 #camera.start_preview()
 for x in range(3, 0, -1):
@@ -29,20 +22,27 @@ for x in range(3, 0, -1):
 camera.capture('/home/pi/Desktop/.image.jpg')
 #camera.stop_preview()
 
-# 1b
+# 1b  ##################################################################
 filename = "pic"
 filename = input("Image Filename (.jpg): ")
 
-# 1c
+# 1c  ##################################################################
 os.system('mv /home/pi/Desktop/.image.jpg /home/pi/Desktop/%s' % filename)
 print("Succesfully created %s on Desktop" % filename) 
 #os.system('echo "Succesfully created %s on Desktop"' % filename) 
 
-# 1d
+# 1d  ##################################################################
 img = cv2.imread('/home/pi/Desktop/1.jpg', 0)
 cv2.imshow('window name here', img)
 cv2.waitKey(0)
-vc2.destroyAllWindows()
+cv2.destroyAllWindows()
 
+
+# 1d  ##################################################################
+
+# 1d  ##################################################################
+# 1d  ##################################################################
+# 1d  ##################################################################
+# 1d  ##################################################################
 
 
