720c815f62b93fee1bcfee5b293bf1735caab7de
rschreib <rschreib@mymail.mines.edu>
2018-02-02 21:14:49 -0700

    finished part 6

--- exercise2/exercise2.py
+++ exercise2/exercise2.py
@@ -18,8 +18,7 @@ camera.rotation = 180
 camera.resolution = (1024, 768) 
 folderpath = "/home/pi/Desktop"         
 filename = "1.jpg"                      #default value
-#mouseX = 0
-#mouseY = 0
+
 global mouseX,mouseY
 
 def countDown():
@@ -44,14 +43,12 @@ def storeFile(name):
     print("Succesfully created %s on Desktop" % name)
 
 def get_pixel_location(event,x,y,flags, param):
-    #global mouseX,mouseY
     if event == cv2.EVENT_LBUTTONDBLCLK:
             mouseX,mouseY = x,y
             print("x:",mouseX,"y:",mouseY)
             print(img[(mouseY,mouseX)])
 
 def get_pixel_location2(event,x,y,flags, param):
-    #global mouseX,mouseY
     if event == cv2.EVENT_LBUTTONDBLCLK:
             mouseX,mouseY = x,y
             print("\nx:",mouseX,"y:",mouseY)
@@ -120,20 +117,16 @@ def function3():
     a = img[(mouseY,mouseX)][0]
     b = img[(mouseY,mouseX)][1]
     c = img[(mouseY,mouseX)][2]
-    #total = a + b + c
         
     for j in range(0,img.shape[0]):
         for i in range(0, img.shape[1]):
             blue = img[(j,i)][0]
             green = img[(j,i)][1]
             red = img[(j,i)][2]
-            #othertotal = blue + green + red
-            #if othertotal > total + 10 or othertotal < total - 10:
-                #img1[(j,i)] = black
             #if abs(a-blue) > 40 or abs(b-green) > 40 or abs(c-red) > 40:
-                #img[(j,i)] = black
             if blue>45 or green>200 or green<70 or red<90 or red>220:
                  img[(j,i)] = black
+                 
     # 3f  ##################################################################
     cv2.imshow('colors-copy.jpg', img1)                        
     cv2.imshow('colors-copy Yellow', img)
@@ -160,19 +153,15 @@ def function4():
     #camera.framerate = 30
     #camera.sharpness = 0
     '''
-    #yellow [0-20],[70-200],[90-220]
-    #blue  [80-140],[0-20],[0-15] 
-    #green [0-15],[90-160],[20-70]
-    #red  [0-15],[0-15], [40-160]
 
     lower_yellow = np.array([20,230,90])
-    upper_yellow = np.array([50, 255, 245])
+    upper_yellow = np.array([50, 256, 245])
 
     lower_blue = np.array([95,210,70])
     upper_blue = np.array([140,256,250])
 
     lower_green = np.array([40,230,100])
-    upper_green = np.array([80,255,200])
+    upper_green = np.array([80,256,210])
 
     lower_red = np.array([0,180,70])
     upper_red = np.array([15,256,241])
@@ -180,19 +169,20 @@ def function4():
     pic1address = '{}/{}.jpg'.format(folderpath,'pic1')
     pic2address = '{}/{}.jpg'.format(folderpath,'pic2')
     pic3address = '{}/{}.jpg'.format(folderpath,'pic3')
+    
     capture('pic1')
     capture('pic2')
     capture('pic3')
+    
     p1 = cv2.imread(pic1address)
     p2 = cv2.imread(pic2address)
     p3 = cv2.imread(pic3address)
     arr = [p1,p2,p3]
-    global hsv
-    #hsv1 = cv2.cvtColor(p1, cv2.COLOR_BGR2HSV)
-    #hsv2 = cv2.cvtColor(p2, cv2.COLOR_BGR2HSV)
-    #hsv3 = cv2.cvtColor(p3, cv2.COLOR_BGR2HSV)
     
-    for p in arr:
+    global hsv
+    
+    for i in range(3):
+        p = arr[i]
         hsv = cv2.cvtColor(p, cv2.COLOR_BGR2HSV)
 
         yellow_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
@@ -219,23 +209,71 @@ def function4():
         cv2.imshow('res1', res1)
         cv2.imshow('res2', res2)
         cv2.imshow('res3', res3)
+
         
-        cv2.imwrite('{}/{}Y.jpg'.format(folderpath,p), res)
-        cv2.imwrite('{}/{}B.jpg'.format(folderpath,p), res1)
-        cv2.imwrite('{}/{}G.jpg'.format(folderpath,p), res2)
-        cv2.imwrite('{}/{}R.jpg'.format(folderpath,p), res3)
+        cv2.imwrite('{}/{}Y.jpg'.format(folderpath,i), res)
+        cv2.imwrite('{}/{}B.jpg'.format(folderpath,i), res1)
+        cv2.imwrite('{}/{}G.jpg'.format(folderpath,i), res2)
+        cv2.imwrite('{}/{}R.jpg'.format(folderpath,i), res3)
         
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+
+def function4_5():
+    file1 = '/home/pi/seed/exercise2/part4images/0B.jpg'
+    file2 = '/home/pi/seed/exercise2/part4images/0G.jpg'
+    file3 = '/home/pi/seed/exercise2/part4images/0R.jpg'
+    file4 = '/home/pi/seed/exercise2/part4images/0Y.jpg'
+
+    img1 = cv2.imread(file1, 0)
+    img2 = cv2.imread(file2, 0)
+    img3 = cv2.imread(file3, 0)
+    img4 = cv2.imread(file4, 0)
+
+    combined = np.array([(img1[i] + img2[i] + img3[i] + img4[i]) for i in range(img1.shape[0])])
+   
+    file5 = '/home/pi/seed/exercise2/part4images/Combined.jpg'
+
+    cv2.imshow('Combined Image', combined)
+    cv2.imwrite(file5, combined)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     
-function4()
+def function5():
+    # 5   ##################################################################
+    file1 = '/home/pi/seed/exercise2/part5/Combined.jpg'
+    file1new = '/home/pi/seed/exercise2/part5/Combinednew.jpg'
+    img = cv2.imread(file1,0)
+    kernel = np.ones((5,5),np.uint8)
+    opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+    erosion = cv2.erode(opening,kernel,iterations = 2)
+    #closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    #dilation = cv2.dilate(img2,kernel,iterations = 1)
 
+    cv2.imshow('Morphed Image', erosion)
+    cv2.imwrite(file1new, erosion)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     
+def function6():
+    # 6   ##################################################################
+    file1 = '/home/pi/seed/exercise2/part5/Combinednew.jpg'
+    file1blurred = '/home/pi/seed/exercise2/part6/CombinednewBlurred.jpg'
+    img = cv2.imread(file1,0)
+    
+    #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# 5   ##################################################################
+    blur = cv2.GaussianBlur(img,(5,5),0)
+    cv2.imshow('Gray Image', blur)
+    cv2.imwrite(file1blurred, blur)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+def function7():
+  
+    
+    # 7a  ##################################################################
 
-# 6   ##################################################################
 
-# 7a  ##################################################################
-# 7b  ##################################################################
-# 7c  ##################################################################
+    # 7b  ##################################################################
+    # 7c  ##################################################################
