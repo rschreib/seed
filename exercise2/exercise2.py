ad162c0af768168702ffaa1e3cd1f47d5ec5c402
rschreib <rschreib@mymail.mines.edu>
2018-02-02 22:49:29 -0700

    commented in the code

--- exercise2/exercise2.py
+++ exercise2/exercise2.py
@@ -13,43 +13,46 @@ import numpy as np
 import cv2
 import glob
 
+#These functions should be called in numerical order in order for them to work.
+#Some manual transfering of picture files into other folders may be necessary.
+
 
 camera = PiCamera()
-camera.rotation = 180
+camera.rotation = 180                   #Initial Camera settings
 camera.resolution = (1024, 768) 
 folderpath = "/home/pi/Desktop"         
 filename = "1.jpg"                      #default value
 
 global mouseX,mouseY
 
-def countDown():
+def countDown():                        #Count down from 3 to 1
     for x in range(3, 0, -1):
         sleep(1)
         print(x)
 
-def captureImage():
+def captureImage():                     #Captures image 
     print("Look into Camera ...")
     #camera.start_preview()
     countDown()
     camera.capture('%s/.image.jpg' % folderpath)
     #camera.stop_preview()
 
-def capture(name):
+def capture(name):                      #Captures image & saves image differently
     print("Look into Camera ...")
     countDown()
     camera.capture('{}/{}.jpg'.format(folderpath,name))
 
-def storeFile(name):
+def storeFile(name):                    #Create file on desktop
     os.system('mv %s/.image.jpg %s/%s' % (folderpath, folderpath, name))
     print("Succesfully created %s on Desktop" % name)
 
-def get_pixel_location(event,x,y,flags, param):
+def get_pixel_location(event,x,y,flags, param):     #returns pixel location and BGR value after clicking on img
     if event == cv2.EVENT_LBUTTONDBLCLK:
             mouseX,mouseY = x,y
             print("x:",mouseX,"y:",mouseY)
             print(img[(mouseY,mouseX)])
 
-def get_pixel_location2(event,x,y,flags, param):
+def get_pixel_location2(event,x,y,flags, param):    #returns pixel location and BGR value after clicking on hsv
     if event == cv2.EVENT_LBUTTONDBLCLK:
             mouseX,mouseY = x,y
             print("\nx:",mouseX,"y:",mouseY)
@@ -57,29 +60,30 @@ def get_pixel_location2(event,x,y,flags, param):
                                 
 def function1():
     # 1a  ##################################################################
-    captureImage()
+    captureImage()      #captures an image
 
     # 1b  ##################################################################
-    filename = input("Image Filename (.jpg): ")
+    filename = input("Image Filename (.jpg): ")   #asks the user for a filename to store the image
     filename = filename + ".jpg"
-    #filename = "1.jpg"
+
     # 1c  ##################################################################
-    storeFile(filename)
+    storeFile(filename)             # stores the captured image using the filename provided by the user
 
     # 1d  ##################################################################
-    print("\nNow select a Pixel on the Image...\n")
+    print("\nNow select a Pixel on the Image...\n") #displays the image on the screen
     global img
     img = cv2.imread('%s/%s' % (folderpath, filename))
   
     # 1e  ##################################################################
-    cv2.namedWindow('Image Captured',1)
+    cv2.namedWindow('Image Captured',1)     #asks the user to select a pixel using the mouse and displays the 3 color values for that pixel
     cv2.setMouseCallback('Image Captured', get_pixel_location)
     cv2.imshow('Image Captured', img)                                        
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
-def function2():
+def function2():    #reads a previously stored image (use an image captured from part 1), resizes 
+                    #the image to half its size without changing the aspect ratio
     # 2   ##################################################################
     img = cv2.imread('/home/pi/Desktop/1.jpg',1)
     small = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation = cv2.INTER_AREA)
@@ -91,21 +95,23 @@ def function2():
 
 def function3():
     # 3a  ##################################################################
-    captureImage()
+    captureImage()              #captures and image
 
     # 3b  ##################################################################
-    storeFile("colors.jpg")
+    storeFile("colors.jpg")     #stores the image as colors.jpg
         
     # 3c  ##################################################################
+                                #makes the original image smaller
     os.system('cp %s/colors.jpg %s/colors-copy.jpg' % (folderpath, folderpath))
 
     # 3d  ##################################################################
-    global img
+    global img                  #makes the original image smaller
     img = cv2.imread('/home/pi/Desktop/colors-copy.jpg',1)
     img = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation = cv2.INTER_AREA)
     cv2.imwrite('/home/pi/Desktop/colors-copy.jpg',img)
         
     # 3e  ##################################################################
+                                #detects the color yellow in this image
     img1 = cv2.imread('/home/pi/Desktop/colors-copy.jpg',1)
     cv2.namedWindow('Image Captured',1)
     cv2.setMouseCallback('Image Captured', get_pixel_location)
@@ -115,20 +121,23 @@ def function3():
     print("\nProcessing Image...")
 
     black = [0,0,0]
-    a = img[(mouseY,mouseX)][0]
-    b = img[(mouseY,mouseX)][1]
-    c = img[(mouseY,mouseX)][2]
-        
-    for j in range(0,img.shape[0]):
+    a = img[(mouseY,mouseX)][0] #image width (number of columns)
+    b = img[(mouseY,mouseX)][1] #image height (number of rows)
+    c = img[(mouseY,mouseX)][2] #dimension size
+    
+                                #loops through each pixel BGR value and changes
+                                # it to black if it is not yellow
+    for j in range(0,img.shape[0]):     
         for i in range(0, img.shape[1]):
             blue = img[(j,i)][0]
             green = img[(j,i)][1]
             red = img[(j,i)][2]
-            #if abs(a-blue) > 40 or abs(b-green) > 40 or abs(c-red) > 40:
             if blue>45 or green>200 or green<70 or red<90 or red>220:
                  img[(j,i)] = black
                  
     # 3f  ##################################################################
+                                #Display the original image and the image after the color
+                                #yellow has been isolated side by side
     cv2.imshow('colors-copy.jpg', img1)                        
     cv2.imshow('colors-copy Yellow', img)
     print("Image Created")
@@ -154,7 +163,12 @@ def function4():
     #camera.framerate = 30
     #camera.sharpness = 0
     '''
+    #Takes three pictures (using the Pi Camera) of the colors slide in different lighting
+    #conditions, at different distances and at different heights. Changes the images
+    #from BGR to the HSV color space. Isolates yellow, blue,
+    #green, and red in all of these images
 
+                    #Define ranges for the pixel colors
     lower_yellow = np.array([20,230,90])
     upper_yellow = np.array([50, 256, 245])
 
@@ -185,12 +199,12 @@ def function4():
     for i in range(3):
         p = arr[i]
         hsv = cv2.cvtColor(p, cv2.COLOR_BGR2HSV)
-
+                                    # makes the pixels white and black everywhere else
         yellow_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
         blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
         green_mask = cv2.inRange(hsv, lower_green, upper_green)
         red_mask = cv2.inRange(hsv, lower_red, upper_red)
-    
+                                    # adds a color tint to the white pixels
         res = cv2.bitwise_and(p, p, mask= yellow_mask)
         res1 = cv2.bitwise_and(p, p, mask= blue_mask)
         res2 = cv2.bitwise_and(p, p, mask= green_mask)
@@ -210,7 +224,6 @@ def function4():
         cv2.imshow('res1', res1)
         cv2.imshow('res2', res2)
         cv2.imshow('res3', res3)
-
         
         cv2.imwrite('{}/{}Y.jpg'.format(folderpath,i), res)
         cv2.imwrite('{}/{}B.jpg'.format(folderpath,i), res1)
@@ -221,6 +234,8 @@ def function4():
         cv2.destroyAllWindows()
 
 def function4_5():
+    # This will take 4 of the isolated colored images and combine them into one image by
+    # adding the numpy.arrays together using matrix addition
     file1 = '/home/pi/seed/exercise2/part4images/0B.jpg'
     file2 = '/home/pi/seed/exercise2/part4images/0G.jpg'
     file3 = '/home/pi/seed/exercise2/part4images/0R.jpg'
@@ -242,6 +257,7 @@ def function4_5():
     
 def function5():
     # 5   ##################################################################
+                                #Cleans up the image by performing morphological transformations.
     file1 = '/home/pi/seed/exercise2/part5/Combined.jpg'
     file1new = '/home/pi/seed/exercise2/part5/Combinednew.jpg'
     img = cv2.imread(file1,0)
@@ -258,12 +274,13 @@ def function5():
     
 def function6():
     # 6   ##################################################################
+                                #Convert the cleaned-up image to a grayscale image
     file1 = '/home/pi/seed/exercise2/part5/Combinednew.jpg'
     file1blurred = '/home/pi/seed/exercise2/part6/CombinednewBlurred.jpg'
     img = cv2.imread(file1,0)
     
     #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+                                #Blurs the image a little bit
     blur = cv2.GaussianBlur(img,(5,5),0)
     cv2.imshow('Gray Image', blur)
     cv2.imwrite(file1blurred, blur)
@@ -274,7 +291,7 @@ def function7():
     # 7a  ##################################################################
     # 7b  ##################################################################
     # 7c  ##################################################################
-    
+                                #Draw contours around the blobs in the image    
     file1 = '/home/pi/seed/exercise2/part6/CombinednewBlurred.jpg'
     file1Contours = '/home/pi/seed/exercise2/part6/CombinednewBlurredCountours.jpg'
     im = cv2.imread(file1, 0)
